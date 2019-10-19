08caed1f6e9e7d2a082d885045b7eabf1173be0f
steven <stevenzamora205@gmail.com>
2019-10-19 19:43:11 -0400

    Added second page functionality

--- CheckWeight.js
+++ CheckWeight.js
@@ -1,16 +1,29 @@
-function formdata() {
-	var firstname1 = document.getElementById('firstname').value;
-	var weight1 = document.getElementById('weight').value;
-	var height1 = document.getElementById('height').value;
-	var age = document.getElementById('age').value;
+var firstname1;
+var realweight1;
+var idealweight1;
+var age1;
 
-	if (age < 17) {
+function formdata() {
+	//Getting the values from the user
+	firstname1 = document.getElementById('firstname').value;
+	realweight1 = document.getElementById('realweight').value;
+	idealweight1 = document.getElementById('idealweight').value;
+	age1 = document.getElementById('age').value;
+	console.log('first ==' + firstname1);
+
+	firstname1 = 'dwafa';
+	//Chechking to see if the user is the right age
+	if (age1 < 17) {
 		alert(firstname1 + ' you are too young for this');
 	}
 
-	if (age > 22) {
+	if (age1 > 22) {
 		alert(firstname1 + ' you are too old for this');
 	}
+}
 
-	alert(firstname1);
+console.log('second ==' + firstname1);
+function myFunction() {
+	console.log('third ==' + firstname1);
+	document.getElementById('myText').innerHTML = firstname1;
 }
